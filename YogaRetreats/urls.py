55c5276b12d40dee5f50dbb2eb3a58390e36370f
redesignpro <90088875+redesignpro@users.noreply.github.com>
2021-09-05 08:38:04 -0400

    Changed URLs.PY

--- YogaRetreats/urls.py
+++ YogaRetreats/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('listings.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG is True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
