748aece20f7d07019e994ab4b9ab4d990980df08
redesignpro <90088875+redesignpro@users.noreply.github.com>
2021-09-06 07:03:57 -0400

    changes to whitenoise settings.py

--- YogaRetreats/settings.py
+++ YogaRetreats/settings.py
@@ -118,7 +118,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'YogaRetreats.wsgi.application'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_ALLOW_ALL_ORIGINS = True
 # STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Database
